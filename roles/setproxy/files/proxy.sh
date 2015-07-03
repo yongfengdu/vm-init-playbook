2baeec5e1abc8276271a7f4c66db666a261b94ee
Yongfeng Du <dolpher.du@intel.com>
2015-07-03 02:24:22 +0000

    iptable rules for docker

--- roles/setproxy/files/proxy.sh
+++ roles/setproxy/files/proxy.sh
@@ -8,9 +8,12 @@ iptables -t nat -A REDSOCKS -d 172.16.0.0/12 -j RETURN
 iptables -t nat -A REDSOCKS -d 192.168.0.0/16 -j RETURN
 iptables -t nat -A REDSOCKS -d 224.0.0.0/4 -j RETURN
 iptables -t nat -A REDSOCKS -d 240.0.0.0/4 -j RETURN
-iptables -t nat -A REDSOCKS -p tcp -j REDIRECT --to-ports 6666
-iptables -t nat -A REDSOCKS -p udp -j REDIRECT --to-ports 8888
+#iptables -t nat -A REDSOCKS -p tcp -j REDIRECT --to-ports 6666
+#iptables -t nat -A REDSOCKS -p udp -j REDIRECT --to-ports 8888
+iptables -t nat -A REDSOCKS -p tcp -j DNAT --to-destination 127.0.0.1:6666
+iptables -t nat -A REDSOCKS -p udp -j DNAT --to-destination 127.0.0.1:8888
 iptables -t nat -A OUTPUT -p tcp -j REDSOCKS
-pkill redsocks
-/usr/sbin/redsocks -c /etc/redsocks.conf
+iptables -t nat -A PREROUTING -p tcp -j REDSOCKS
+#pkill redsocks
+#/usr/sbin/redsocks -c /etc/redsocks.conf
 
